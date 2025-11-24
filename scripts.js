8e5ca11f7396bf9eaa40218d5bb48758c23f7332
bjvvrn <bjvvrn@gmail.com>
2025-11-24 11:45:55 -0500

    Create getHumanChoice function
    Add a function that allows the user to input their choice.

--- scripts.js
+++ scripts.js
@@ -10,3 +10,8 @@ function getComputerChoice() {
             return 'scissors';
     }
 }
+
+function getHumanChoice() {
+    const humanChoice = prompt('Rock, paper or scissors?');
+    return humanChoice;
+}
