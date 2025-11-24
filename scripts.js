be1201657678a05cc188bc613ccf7871f3f13c49
bjvvrn <bjvvrn@gmail.com>
2025-11-24 13:21:33 -0500

    Create playRound function
    This function plays a single round and increments the round winner's
    score and logs a winner announcement.

--- scripts.js
+++ scripts.js
@@ -16,5 +16,57 @@ function getHumanChoice() {
     return humanChoice.toLocaleLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case 'paper':
+            switch (computerChoice) {
+                case 'paper':
+                    console.log('Its a tie!');
+                    break;
+                case 'rock':
+                    console.log('You win! Paper beats Rock');
+                    humanScore++;
+                    break;
+                case 'scissors':
+                    console.log('You lose! Scissors beats Paper');
+                    computerScore++;
+                    break;
+            }
+            break;
+
+        case 'rock':
+            switch (computerChoice) {
+                case 'rock':
+                    console.log('Its a tie!');
+                    break;
+                case 'paper':
+                    console.log('You lose! Paper beats Rock');
+                    computerScore++;
+                    break;
+                case 'scissors':
+                    console.log('You win! Rock beats Scissors');
+                    humanScore++;
+                    break;
+            }
+            break;
+
+        case 'scissors':
+            switch (computerChoice) {
+                case 'scissors':
+                    console.log('Its a tie!');
+                    break;
+                case 'paper':
+                    console.log('You win! Scissors beats paper');
+                    humanScore++;
+                    break;
+                case 'rock':
+                    console.log('You lose! Rock beats Scrissors');
+                    computerScore++;
+                    break;
+            }
+            break;
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
