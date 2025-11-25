121a7340a5e9457be1c3231b1952785c564de3ec
bjvvrn <bjvvrn@gmail.com>
2025-11-25 08:41:29 -0500

    Create playGame function
    This function calls playRound() to play 5 rounds and keeps trakc of the
    scores.

--- scripts.js
+++ scripts.js
@@ -16,57 +16,67 @@ function getHumanChoice() {
     return humanChoice.toLocaleLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    switch (humanChoice) {
-        case 'paper':
-            switch (computerChoice) {
-                case 'paper':
-                    console.log('Its a tie!');
-                    break;
-                case 'rock':
-                    console.log('You win! Paper beats Rock');
-                    humanScore++;
-                    break;
-                case 'scissors':
-                    console.log('You lose! Scissors beats Paper');
-                    computerScore++;
-                    break;
-            }
-            break;
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        switch (humanChoice) {
+            case 'paper':
+                switch (computerChoice) {
+                    case 'paper':
+                        console.log('Its a tie!');
+                        break;
+                    case 'rock':
+                        console.log('You win! Paper beats Rock');
+                        humanScore++;
+                        break;
+                    case 'scissors':
+                        console.log('You lose! Scissors beats Paper');
+                        computerScore++;
+                        break;
+                }
+                break;
 
-        case 'rock':
-            switch (computerChoice) {
-                case 'rock':
-                    console.log('Its a tie!');
-                    break;
-                case 'paper':
-                    console.log('You lose! Paper beats Rock');
-                    computerScore++;
-                    break;
-                case 'scissors':
-                    console.log('You win! Rock beats Scissors');
-                    humanScore++;
-                    break;
-            }
-            break;
+            case 'rock':
+                switch (computerChoice) {
+                    case 'rock':
+                        console.log('Its a tie!');
+                        break;
+                    case 'paper':
+                        console.log('You lose! Paper beats Rock');
+                        computerScore++;
+                        break;
+                    case 'scissors':
+                        console.log('You win! Rock beats Scissors');
+                        humanScore++;
+                        break;
+                }
+                break;
 
-        case 'scissors':
-            switch (computerChoice) {
-                case 'scissors':
-                    console.log('Its a tie!');
-                    break;
-                case 'paper':
-                    console.log('You win! Scissors beats paper');
-                    humanScore++;
-                    break;
-                case 'rock':
-                    console.log('You lose! Rock beats Scrissors');
-                    computerScore++;
-                    break;
-            }
-            break;
+            case 'scissors':
+                switch (computerChoice) {
+                    case 'scissors':
+                        console.log('Its a tie!');
+                        break;
+                    case 'paper':
+                        console.log('You win! Scissors beats paper');
+                        humanScore++;
+                        break;
+                    case 'rock':
+                        console.log('You lose! Rock beats Scrissors');
+                        computerScore++;
+                        break;
+                }
+                break;
+        }
     }
-}
 
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+
+    console.log(humanScore, computerScore);
+}
