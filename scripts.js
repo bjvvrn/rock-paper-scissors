b89a28a473d5175e257c419f732a1013c9a67dff
bjvvrn <bjvvrn@gmail.com>
2025-11-25 08:53:53 -0500

    Log human and computer scores
    Now playRound() shows the in-game score.

--- scripts.js
+++ scripts.js
@@ -23,14 +23,17 @@ function playGame() {
                 switch (computerChoice) {
                     case 'paper':
                         console.log('Its a tie!');
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'rock':
                         console.log('You win! Paper beats Rock');
                         humanScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'scissors':
                         console.log('You lose! Scissors beats Paper');
                         computerScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                 }
                 break;
@@ -39,14 +42,17 @@ function playGame() {
                 switch (computerChoice) {
                     case 'rock':
                         console.log('Its a tie!');
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'paper':
                         console.log('You lose! Paper beats Rock');
                         computerScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'scissors':
                         console.log('You win! Rock beats Scissors');
                         humanScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                 }
                 break;
@@ -55,14 +61,17 @@ function playGame() {
                 switch (computerChoice) {
                     case 'scissors':
                         console.log('Its a tie!');
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'paper':
                         console.log('You win! Scissors beats paper');
                         humanScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                     case 'rock':
                         console.log('You lose! Rock beats Scrissors');
                         computerScore++;
+                        console.log(`human: ${humanScore}, computer: ${computerScore}`);
                         break;
                 }
                 break;
@@ -77,6 +86,4 @@ function playGame() {
     playRound(getHumanChoice(), getComputerChoice());
     playRound(getHumanChoice(), getComputerChoice());
     playRound(getHumanChoice(), getComputerChoice());
-
-    console.log(humanScore, computerScore);
 }
